2f1ef30410bc860ff3c0114086a0a2ee587fdc30
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-06-21 20:58:27 -0400

    commit tree 6921abe28bcffbd249a0d532a62299776112c65e

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((a) => (
+        <div>
+          <p>{a.name}</p>
+          <ul>
+            {a.movies.map((m) => (
+              <li>{m}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+
+      {directors.map((d) => (
+        <div>
+          <p>{d.name}</p>
+          <ul>
+            {d.movies.map((m) => (
+              <li>{m}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
